c088327b3298c2935395908f45ec3caeaf66bd88
ChristopheSim <c.christophesimon@gmail.com>
2018-12-11 16:00:17 +0100

    Add the autoincrement parts

--- create_db2.py
+++ create_db2.py
@@ -8,7 +8,7 @@
 import utils
 from sqlalchemy import Column, ForeignKey, inspect
 from sqlalchemy import Integer, String, Date
-from sqlalchemy_utils import database_exists
+from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -28,7 +28,8 @@ else:
 class Person(Base):
     __tablename__ = 'person'
 
-    pk_person = Column(Integer, primary_key=True, nullable=False)
+    pk_person = Column(Integer, primary_key=True, nullable=False,
+    autoincrement=True)
     firstname = Column(String(30))
     lastnamename = Column(String(30))
     birthdate = Column(Date, nullable=False)
@@ -39,16 +40,18 @@ class Person(Base):
 class Role(Base):
     __tablename__ = 'role'
 
-    pk_role = Column(Integer, primary_key=True, nullable=False)
+    pk_role = Column(Integer, primary_key=True, nullable=False,
+    autoincrement=True)
     name = Column(String(20), nullable=False)
     links = relationship("Link", back_populates="role")
 
 class Movie(Base):
     __tablename__ = 'movie'
 
-    pk_movie = Column(Integer, primary_key=True, nullable=False)
+    pk_movie = Column(Integer, primary_key=True, nullable=False,
+    autoincrement=True)
     date = Column(Date, nullable=False)
-    time = Column(Integer, nullable=False)
+    time = Column(Integer, nullable=False, autoincrement=False)
     title = Column(String(30), nullable=False)
     links = relationship("Link", back_populates="movie")
 
@@ -56,10 +59,14 @@ class Movie(Base):
 class Link(Base):
     __tablename__ = 'link'
 
-    pk_link = Column(Integer, primary_key=True, nullable=False)
-    fk_movie = Column(Integer, ForeignKey('movie.pk_movie'), nullable=False)
-    fk_person = Column(Integer, ForeignKey('person.pk_person'), nullable=False)
-    fk_role = Column(Integer, ForeignKey('role.pk_role'), nullable=False)
+    pk_link = Column(Integer, primary_key=True, nullable=False,
+    autoincrement=True)
+    fk_movie = Column(Integer, ForeignKey('movie.pk_movie'), nullable=False,
+    autoincrement=False)
+    fk_person = Column(Integer, ForeignKey('person.pk_person'), nullable=False,
+    autoincrement=False)
+    fk_role = Column(Integer, ForeignKey('role.pk_role'), nullable=False,
+    autoincrement=False)
     movie = relationship("Movie", back_populates="links")
     role = relationship("Role", back_populates="links")
     person = relationship("Person", back_populates="links")
